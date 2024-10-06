ff4f2d1fcf58a7825eebda8f232325d8697bb3b1
Christopher Joshy <144864811+ChristopherJoshy@users.noreply.github.com>
2024-10-06 17:55:10 +0530

    Update cj.sh

--- cj.sh
+++ cj.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 
 detect_distro() {
-    if [[ "$OSTYPE" == linux-android* ]]; then
-            distro="termux"
-    fi
+    case "$OSTYPE" in
+        linux-android*) distro="termux" ;;
+        darwin) distro="darwin" ;;
+        *) distro=$(ls /etc | awk 'match($0, "(.+?)[-_](?:release|version)", groups) {if(groups[1] != "os") print groups[1}') ;;
+    esac
 
     if [ -z "$distro" ]; then
-        distro=$(ls /etc | awk 'match($0, "(.+?)[-_](?:release|version)", groups) {if(groups[1] != "os") {print groups[1]}}')
-    fi
-
-    if [ -z "$distro" ]; then
-        if [ -f "/etc/os-release" ]; then
-            distro="$(source /etc/os-release && echo $ID)"
-        elif [ "$OSTYPE" == "darwin" ]; then
-            distro="darwin"
-        else 
-            distro="invalid"
-        fi
+        [ -f "/etc/os-release" ] && source /etc/os-release && distro="$ID"
+        [ -z "$distro" ] && distro="invalid"
     fi
 }
 
 pause() {
     read -n1 -r -p "Press any key to continue..." key
 }
+
 banner() {
     clear
-    echo -e "Introducing CJ BOMBER"
-    echo -e "This Bomber Was Created By CJ"
+    echo -e "Introducing CJ BOMBER\nThis Bomber Was Created By CJ"
 }
 
-init_environ(){
-    declare -A backends; backends=(
+init_environ() {
+    declare -A backends=(
         ["arch"]="pacman -S --noconfirm"
         ["debian"]="apt-get -y install"
         ["ubuntu"]="apt -y install"
@@ -44,30 +37,23 @@ init_environ(){
     )
 
     INSTALL="${backends[$distro]}"
-
-    if [ "$distro" == "termux" ]; then
-        PYTHON="python"
-        SUDO=""
-    else
-        PYTHON="python3"
-        SUDO="sudo"
-    fi
+    PYTHON="${distro}" == "termux" ? "python" : "python3"
+    SUDO="$([[ "$distro" == "termux" ]] && echo "" || echo "sudo")"
     PIP="$PYTHON -m pip"
 }
 
-install_deps(){
-    
+install_deps() {
     packages=(openssl git $PYTHON $PYTHON-pip figlet toilet)
-    if [ -n "$INSTALL" ];then
-        for package in ${packages[@]}; do
+
+    if [ -n "$INSTALL" ]; then
+        for package in "${packages[@]}"; do
             $SUDO $INSTALL $package
         done
         $PIP install -r requirements.txt
     else
-        echo "We could not install dependencies."
-        echo "Please make sure you have git, python3, pip3 and requirements installed."
-        echo "Then you can execute bomber.py ."
-        exit
+        echo "Dependencies could not be installed."
+        echo "Ensure git, python3, pip3, and requirements are installed."
+        exit 1
     fi
 }
 
@@ -75,51 +61,44 @@ banner
 pause
 detect_distro
 init_environ
-if [ -f .update ];then
+
+if [ -f .update ]; then
     echo "All Requirements Found...."
 else
     echo 'Installing Requirements....'
-    echo .
-    echo .
     install_deps
-    echo This Script Was Made By yo > .update
+    echo "This Script Was Made By yo" > .update
     echo 'Requirements Installed....'
     pause
 fi
-while :
-do
-    banner
-    echo -e " Please Read Instruction Carefully !!! "
-    echo " "
-    echo "Press 1 To  Start SMS  Bomber "
-    echo "Press 2 To  Start CALL Bomber (Working alla)"
-    echo "Press 3 To  Start MAIL Bomber (Working alla)"
-    echo "Press 4  To Update the Providers List (increase messaging power) "
-    echo "Press 5 To  Exit "
-    read ch
-    clear
-    if [ $ch -eq 1 ];then
-        $PYTHON bomber.py --sms
-        exit
-    elif [ $ch -eq 2 ];then
-        $PYTHON bomber.py --call
-        exit
-    elif [ $ch -eq 3 ];then
-        $PYTHON bomber.py --mail
-        exit
-    elif [ $ch -eq 4 ];then
-        echo -e "Downloading Latest Files..."
-        rm -f .update
-        $PYTHON bomber.py --update
-        echo -e "RUN CJ BOMBER Again..."
-        pause
-        exit
-    elif [ $ch -eq 5 ];then
-        banner
-        exit
-    else
-        echo -e " Invalid Input !!!"
-        pause
-    fi
-done
 
+while true; do
+    banner
+    echo -e " Please Read Instruction Carefully !!!\n"
+    echo "Press 1 To Start SMS Bomber"
+    echo "Press 2 To Start CALL Bomber (Working alla)"
+    echo "Press 3 To Start MAIL Bomber (Working alla)"
+    echo "Press 4 To Update the Providers List (increase messaging power)"
+    echo "Press 5 To Exit"
+    read -r ch
+    clear
+
+    case $ch in
+        1) $PYTHON bomber.py --sms; exit ;;
+        2) $PYTHON bomber.py --call; exit ;;
+        3) $PYTHON bomber.py --mail; exit ;;
+        4)
+            echo -e "Downloading Latest Files..."
+            rm -f .update
+            $PYTHON bomber.py --update
+            echo -e "RUN CJ BOMBER Again..."
+            pause
+            exit
+            ;;
+        5)
+            banner
+            exit
+            ;;
+        *) echo -e "Invalid Input !!!"; pause ;;
+    esac
+done
